fad929b5f3b5b6d07bd81fff5370e5b300ba0d5a
Adam Cooman <adam.cooman@imec.be>
2024-09-17 15:00:46 +0200

    implemented alternative convert function

--- +yaml/dump.m
+++ +yaml/dump.m
@@ -47,7 +47,7 @@ initSnakeYaml
 import org.yaml.snakeyaml.*;
 
 try
-    javaData = convert(data);
+    data = convert(data,NULL_PLACEHOLDER);
 catch exception
     if string(exception.identifier).startsWith("yaml:dump")
         error(exception.identifier, exception.message);
@@ -56,113 +56,124 @@ catch exception
 end
 dumperOptions = DumperOptions();
 setFlowStyle(dumperOptions, style);
-result = Yaml(dumperOptions).dump(javaData);
+result = Yaml(dumperOptions).dump(data);
 result = string(result).replace(NULL_PLACEHOLDER, "null");
 
-    function result = convert(data)
-        if iscell(data)
-            result = convertCell(data);
-        elseif ischar(data) && isvector(data)
-            result = convertString(data);
-        elseif ~isscalar(data)
-            result = convertArray(data);
-        elseif isstruct(data)
-            result = convertStruct(data);
-        elseif isfloat(data)
-            result = java.lang.Double(data);
-        elseif isa(data, "int64")
-            result = java.lang.Long(data);
-        elseif isa(data, "uint32") || isa(data, "uint64")
-            hexStr = dec2hex(data);
-            result = java.math.BigInteger(hexStr, 16);
-        elseif isinteger(data)
-            result = java.lang.Integer(data);
-        elseif islogical(data)
-            result = java.lang.Boolean(data);
-        elseif isstring(data)
-            result = convertString(data);
-        elseif isdatetime(data)
-            data.Format = "uuuu-MM-dd'T'HH:mm:ss.SSS";
-            result = string(data);
-        elseif yaml.isNull(data)
-            result = java.lang.String(NULL_PLACEHOLDER);
-        else
-            error("yaml:dump:TypeNotSupported", "Data type '%s' is not supported.", class(data))
-        end
-    end
-
-    function result = convertString(data)
-        if contains(data, NULL_PLACEHOLDER)
-            error("yaml:dump:NullPlaceholderNotAllowed", "Strings must not contain '%s' since it is used as a placeholder for null values.", NULL_PLACEHOLDER)
-        end
-        result = java.lang.String(data);
-    end
-
-    function result = convertStruct(data)
-        result = java.util.LinkedHashMap();
-        for key = string(fieldnames(data))'
-            value = convert(data.(key));
-            result.put(key, value);
-        end
-    end
-
-    function result = convertCell(data)
-        data = nest(data);
-        result = java.util.ArrayList();
-        for i = 1:length(data)
-            result.add(convert(data{i}));
-        end
-    end
-
-    function result = convertArray(data)
-        result = convertCell(num2cell(data));
-    end
-
-    function result = nest(data)
-        if isvector(data) || isempty(data)
-            result = data;
-            return
-        end
-        n = size(data, 1);
-        nDimensions = length(size(data));
-        result = cell(1, n);
-        if nDimensions == 2
-            for i = 1:n
-                result{i} = data(i, :);
-            end
-        elseif nDimensions == 3
-            for i = 1:n
-                result{i} = squeeze(data(i, :, :));
-            end
-        else
-            error("yaml:dump:HigherDimensionsNotSupported", "Arrays with more than three dimensions are not supported. Use nested cells instead.")
-        end
-    end
-
-    function initSnakeYaml
-        snakeYamlFile = fullfile(fileparts(mfilename('fullpath')), 'snakeyaml', 'snakeyaml-1.30.jar');
-        if ~ismember(snakeYamlFile, javaclasspath('-dynamic'))
-            javaaddpath(snakeYamlFile);
-        end
-    end
-
-    function setFlowStyle(options, style)
-        import org.yaml.snakeyaml.*;
-        if style == "auto"
-            return
-        end
-        classes = options.getClass.getClasses;
-        classNames = arrayfun(@(c) string(c.getName), classes);
-        styleClassIndex = find(classNames.endsWith("$FlowStyle"), 1);
-        if isempty(styleClassIndex)
-            error("yaml:dump:FlowStyleSelectionFailed", "Unable to select flow style '%s'.", style);
-        end
-        styleFields = classes(styleClassIndex).getDeclaredFields();
-        styleIndex = find(arrayfun(@(f) string(f.getName).lower == style, styleFields));
-        if isempty(styleIndex)
-            error("yaml:dump:FlowStyleSelectionFailed", "Unable to select flow style '%s'.", style);
-        end
-        options.setDefaultFlowStyle(styleFields(styleIndex).get([]));
-    end
-
 end
+
+function data = convert(data,NULL_PLACEHOLDER)
+if isempty(data)
+    data = java.util.ArrayList();
+    return
+end
+if ischar(data)
+    data = string(data);
+end
+data = nest(data);
+class_name = class(data);
+switch class_name
+    case {"double" "logical" "single" "int8" "int16" "int32" "int64"}
+        % nothing to do
+    case "string"
+        if any(contains(data, NULL_PLACEHOLDER))
+            error("yaml:dump:NullPlaceholderNotAllowed", ...
+                "Strings must not contain '%s' since it is used as a placeholder for null values.", NULL_PLACEHOLDER)
+        end
+    case {"uint8" "uint16" "uint32" "uint64"}
+        if isscalar(data)
+            data = java.math.BigInteger(dec2hex(data), 16);
+        else
+            data_copy = data;
+            data = javaArray("java.math.BigInteger",size(data));
+            data_copy = dec2hex(data_copy);
+            for ind = 1 : numel(data_copy)
+                data(ind) = java.math.BigInteger(data_copy(ind,:), 16);
+            end
+        end
+    case "cell"
+        data_copy = data;
+        data = java.util.ArrayList();
+        for i = 1:numel(data_copy)
+            data.add(convert(data_copy{i},NULL_PLACEHOLDER));
+        end
+    case "struct"
+        data_copy = data;
+        if isscalar(data)
+            data = java.util.LinkedHashMap();
+            for key = string(fieldnames(data_copy))'
+                value = convert(data_copy.(key),NULL_PLACEHOLDER);
+                data.put(key, value);
+            end
+        else
+            data = cell(size(data_copy));
+            keys = string(fieldnames(data_copy))';
+            for i = 1 : numel(data_copy)
+                temp = java.util.LinkedHashMap();
+                for key = keys
+                    temp.put(key,convert(data_copy(i).(key),NULL_PLACEHOLDER));
+                end
+                data{i} = temp;
+            end
+        end
+    case "datetime"
+        data.Format = "uuuu-MM-dd'T'HH:mm:ss.SSS";
+        data = string(data);
+    case "yaml.Null"
+        data = java.lang.String(NULL_PLACEHOLDER);
+    otherwise
+        error("yaml:dump:TypeNotSupported", ...
+            "Data type '%s' is not supported.", class(data))
+end
+end
+
+function data = nest(data)
+if isvector(data)
+    return
+end
+data_copy = data;
+siz = size(data);
+siz_1 = siz(1);
+nDimensions = numel(siz);
+data = cell(1, siz_1);
+if nDimensions == 2
+    for i = 1:siz_1
+        data{i} = data_copy(i, :);
+    end
+elseif nDimensions == 3
+    for i = 1:siz_1
+        data{i} = squeeze(data_copy(i, :, :));
+    end
+else
+    error("yaml:dump:HigherDimensionsNotSupported", ...
+        "Arrays with more than three dimensions are not supported."+ ...
+        " Use nested cells instead.")
+end
+end
+
+function initSnakeYaml
+snakeYamlFile = fullfile(fileparts(mfilename('fullpath')), 'snakeyaml', 'snakeyaml-1.30.jar');
+if ~ismember(snakeYamlFile, javaclasspath('-dynamic'))
+    javaaddpath(snakeYamlFile);
+end
+end
+
+function setFlowStyle(options, style)
+import org.yaml.snakeyaml.*;
+if style == "auto"
+    return
+end
+classes = options.getClass.getClasses;
+classNames = arrayfun(@(c) string(c.getName), classes);
+styleClassIndex = find(classNames.endsWith("$FlowStyle"), 1);
+if isempty(styleClassIndex)
+    error("yaml:dump:FlowStyleSelectionFailed", "Unable to select flow style '%s'.", style);
+end
+styleFields = classes(styleClassIndex).getDeclaredFields();
+styleIndex = find(arrayfun(@(f) string(f.getName).lower == style, styleFields));
+if isempty(styleIndex)
+    error("yaml:dump:FlowStyleSelectionFailed", "Unable to select flow style '%s'.", style);
+end
+options.setDefaultFlowStyle(styleFields(styleIndex).get([]));
+end
+
+
